2ad93cf199cce9751aae0cfa28318386dfdf9ac4
Spinu Gheorghe <gheorghespinu2001@gmail.com>
2022-02-04 15:30:00 +0200

    Start

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using APP_1.BinaryGap;
+using APP_1.CyclinRotation;
 
 //using APP_1.BinaryGap;
 
@@ -9,8 +10,22 @@ namespace APP_1
     {
         static void Main(string[] args)
         {
-            int result1 = Task1.task1(1041);
-            Console.WriteLine(result1);
+            // Raspunsul pentru task-ul nr.1
+            // int result1 = Task1.task1(1041);
+            // Console.WriteLine(result1);
+
+            // Raspunsul pentru task-ul nr.2
+            //  int[]  A = {3, 8, 9, 7, 6};
+           //  // int k = 3;
+           //
+           //  Console.Write("Our String is: \n");
+           //  for (int i = 0 ; i < A.Length; i++) 
+           //  {
+           //      Console.Write(A[i] + " ");
+           //  }
+           //
+           //  int[] result2 = Task2.solution(A, 3);
+           // Console.WriteLine(result2);
         }
     }
 }
