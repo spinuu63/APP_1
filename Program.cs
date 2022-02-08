75756e473e520cb09124c6a654e5e8cc72f81dc6
Spinu Gheorghe <gheorghespinu2001@gmail.com>
2022-02-08 12:44:07 +0200

    Hello

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using APP_1.BinaryGap;
 using APP_1.CyclinRotation;
 using APP_1.FrogJmp;
+using System.Linq;
 
 //using APP_1.BinaryGap;
 
@@ -55,8 +56,48 @@ namespace APP_1
         //     return unapairednumber;
 
         // Raspunsul pentru task-ul nr.4
-        int result4 = Task4.sol(10, 85, 2);
-        Console.WriteLine(result4);
+        // int result4 = Task4.sol(10, 85, 2);
+        // Console.WriteLine(result4);
+        
+        // Raspunsul programului nr.5
+        // int[]  Arr = {2, 3, 1,4, 6};
+        // for (int i = 0; i < Arr.Length; i++)
+        // {
+        //     Console.Write(Arr[i] + "\t");
+        // }
+        //
+        // Console.WriteLine();
+        // Array.Sort(Arr);
+        // foreach (var i  in Arr)
+        // {
+        //     Console.Write(i + "\t");
+        // }
+        //
+        // Console.WriteLine("\n\n" + "The missing digit in this string is:\n " + Solution(Arr));
+        // }
+        //
+        // public static int Solution(int[] Arr)
+        // {
+        //     return (int)((Arr.Length == 0) ? 1 : ((long)(Arr.Length + 1) * (Arr.Length + 2) / 2) - Arr.Sum(x => (long)x));
+
+        // Raspunsul programului nr.7
+        // int n = 5, index = 0;
+        // int[] arr7 = {1, 3, 1, 4, 2, 3, 5, 3};
+        // for (int i = 0; i < arr7.Length; i++)
+        // {
+        //     Console.Write ( arr7[i] + "\t");
+        //     if (arr7[i] == n)
+        //     {
+        //         index++;
+        //     }
+        // }
+        // int t = 0;
+        // do
+        // {
+        //     t++;
+        // } while (arr7[t] < n);
+        // Console.WriteLine(t);
+
         }
     }
 }
